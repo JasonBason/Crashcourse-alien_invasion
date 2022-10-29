84b3d12ed9d3899428d61187185a562244c2b5e5
JasonBason <jsh339@cornell.edu>
2022-10-29 11:09:45 -0400

    lots of work

---
+++ Project-Alien_Invasion/Practice_problems/Problem_12-4.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Rocket_test:
+    def __init__(self):
+        """
+        Start the rocket test
+        """
+        self.screen = pygame.display
+        
+    def run_game(self):
+        """
+        Run the game
+        """
+        while True:
+            
+        
